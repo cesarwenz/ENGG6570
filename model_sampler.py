ed3f1b4566d91d445937d19ea8ef33e778371fe8
cesarwenz <77589429+cesarwenz@users.noreply.github.com>
2021-08-09 12:23:28 -0400

    add metrics and filters to model_sampler

--- model_sampler.py
+++ model_sampler.py
@@ -6,10 +6,14 @@ from keras.models import load_model
 from numpy import load
 from numpy import vstack
 from matplotlib import pyplot
+# from kalman_filter_kitti import kalman_filter_kitti
 from numpy.random import randint
 from numpy.random import randn
+import utils
 import cv2
 import numpy as np
+from scipy.signal import lfilter
+from scipy.signal import butter
 
 # Overlay trajectories (data_y) to the image (data_x)
 def create_trajectory(data_x, data_y, obs_len=10):
@@ -100,14 +104,13 @@ def plot_images(X_realA, X_fakeB, X_realB, filename, n_samples=1):
     # save plot to file
     pyplot.subplots_adjust(hspace=0.4)
     pyplot.show()
-    pyplot.savefig(filename)
+    # pyplot.savefig(filename)
     
     # save the generator model
 
 #%%
 
 # load dataset
-
 data = load('data/dual_condition_dataset_test.npz')
 # unpack arrays
 X1, X2, X3 = data['arr_0'], data['arr_1'], data['arr_2']
@@ -115,7 +118,7 @@ X3 = X3-1
 
 #%%
 # load model
-model = load_model('model_056010.h5')
+model = load_model('visual_attention/visual_attention_b14_model_003600.h5')
 
 #%%
 
@@ -128,4 +131,111 @@ src_image, tar_traj, label = X1[ix].reshape((1, 256, 256, 3)), X2[ix].reshape((1
 # generate image from source
 gen_traj = model.predict([src_image, z_input, label])
 plot_images(src_image, gen_traj, tar_traj, 'sample20.png')
+#%%
+# Filtered gen_traj
 
+# define butter filter
+b, a = butter(3, [.5], 'lowpass')
+x = gen_traj.reshape((40,3))[:, 0]
+y = gen_traj.reshape((40,3))[:, 1]
+z = gen_traj.reshape((40,3))[:, 2]
+# Filter each axis
+z = lfilter(b,a,z)
+y = lfilter(b,a,y)
+x = lfilter(b,a,x)
+filtered_traj = np.vstack((x,y,z)).T
+
+# plot filtered model
+plot_images(src_image, filtered_traj.reshape((1,40,3)), tar_traj, 'sample20.png')
+#%%
+import math
+def euclidean_distance(p1, p2):
+    return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2)
+
+#params: traj1, traj2 - sets of two trajectories which the ADE is requires
+#returns: Average Displacement Error between two trajectories
+#brief: takes two traj. of same size, returns the average euclidean distance between all points of traj1 and traj2
+def ADE(traj1, traj2):
+    try:
+        assert(traj1.shape == traj2.shape)
+    except AssertionError:
+        print("trajectory arrays differ in size!")
+        return None
+    dist = []
+    points = len(traj1[0])
+    #print(f'{points} long')
+    for itr in range(points):
+        p1 = traj1[0][itr]
+        p2 = traj2[0][itr]
+        dist.append(euclidean_distance(p1, p2))
+        #print(f'traj1: {p1}, traj2: {p2}, dist: {dist[itr]}')
+    ade = sum(dist)/len(dist)
+    #print(ade)
+    return ade
+
+
+#params: traj1, traj2 - sets of two trajectories which the FDE is requires
+#returns: Final Displacement Error between two trajectories
+#brief: takes two traj. of same size, returns the euclidean distance between final points of traj1 and traj2
+def FDE(traj1, traj2):
+    try:
+        assert(traj1.shape == traj2.shape)
+    except AssertionError:
+        print("trajectory arrays differ in size!")
+        return None
+    last_idx = len(traj1[0]) - 1
+    #print(f'last index is {last_idx}')
+    p1 = traj1[0][last_idx]
+    p2 = traj2[0][last_idx]
+    fde = euclidean_distance(p1, p2)
+    #print(f'traj1: {p1}, traj2: {p2}, dist: {fde}')
+    #print(fde)
+    return fde
+
+# from scipy.signal import lfilter
+# from scipy.signal import butter
+def get_stats(num_trajs=100, latent_dim=512, z_input=generate_latent_points(latent_dim, 1)):
+    minADE = float('inf')
+    minFDE = float('inf')
+    avgADE = []
+    avgFDE = []
+    for img in range(num_trajs):
+        ix = img
+        src_image, tar_traj, label = X1[ix].reshape((1, 256, 256, 3)), X2[ix].reshape((1,40,3)), X3[ix].reshape(1)
+        # generate image from source
+        gen_traj = model.predict([src_image, z_input, label])
+
+        b, a = butter(3, [.5], 'lowpass')
+
+        x = gen_traj.reshape((40,3))[:, 0]
+        y = gen_traj.reshape((40,3))[:, 1]
+        z = gen_traj.reshape((40,3))[:, 2]
+        z = lfilter(b,a,z)
+        y = lfilter(b,a,y)
+        x = lfilter(b,a,x)
+        filtered_traj = np.vstack((x,y,z)).T.reshape((1,40,3))
+        # plot_images(src_image, gen_traj, tar_traj, 'sample6.png')
+        fde = FDE(tar_traj, gen_traj)
+        ade = ADE(tar_traj, gen_traj)
+        # print(f'image: {img}, FDE: {fde}, ADE: {ade}')
+        
+        #minADE
+        minADE = ade if ade < minADE else minADE
+        
+        #minFDE
+        minFDE = fde if fde < minFDE else minFDE
+        
+        #avgADE
+        avgADE.append(ade)
+        
+        #avgFDE
+        avgFDE.append(fde)
+        
+    return(minADE, minFDE, sum(avgADE)/len(avgADE), sum(avgFDE)/len(avgFDE))
+#%%
+%%time
+minADE, minFDE, avgADE, avgFDE = get_stats(num_trajs=4204)
+print(f'minADE = {minADE}')
+print(f'minFDE = {minFDE}')
+print(f'avgADE = {avgADE}')
+print(f'avgFDE = {avgFDE}')
